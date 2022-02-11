d0178d2dc8eb1e89f4f0597ce8413a3d50454b25
mariumariu <mariumariu@gmail.com>
2022-02-11 11:42:45 -0400

    master

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
-app.get('//sum',(req,res) => {
+app.get('/sum',(req,res) => {
     const sum = 1 + 2;
-    res.send(sum);
+    res.send(sum.toString());
 })
 app.listen(3001,() => {
      console.log(`app is listening to port 3001`);
