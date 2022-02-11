4ab2cdc5ed0b4068d0a86ca7a4921c488a4fa373
Ma. Eugenia Quintero Basante <mariumariu@gmail.com>
2022-02-11 01:03:47 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('//sum',(req,res) => {
+    const sum = 1 + 2;
+    res.send(sum);
+})
+app.listen(3001,() => {
+     console.log(`app is listening to port 3001`);
+})
