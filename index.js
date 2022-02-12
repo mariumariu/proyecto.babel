0f811cc9d420e28a6f79afe7bb4351d27a017b60
mariumariu <mariumariu@gmail.com>
2022-02-11 23:51:08 -0400

    cambios para rick & morty

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+const plumbus = require('rickmortyapi');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/sum',(req,res) => {
-    const sum = 1 + 2;
-    res.send(sum.toString());
+app.get('/rm',async(req,res) => {
+    const episodes = await got.get('https://rickandmortyapi.com/api/episode/10');
+    res.json(episodes);
+    console.log(episodes)
 })
 app.listen(3001,() => {
      console.log(`app is listening to port 3001`);
